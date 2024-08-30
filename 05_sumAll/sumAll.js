c267b0ff5a32486b5168caf5cc778715b999839b
giangkevin <149978467+giangkevin@users.noreply.github.com>
2024-08-30 15:21:27 +0200

    Add 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(minimum, maximum) {
+    if(minimum < 0  || maximum < 0) return("Error");
+    let total = 0;
+    for(i = minimum; i <= maximum; i++){
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
